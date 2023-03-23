f851c651c6c5937b13f938c2b4e8de30414d2778
Chuanrui WANG <dragon.wcr12@gmail.com>
2023-03-23 14:57:17 -0600

    reduce to smaller test set

--- random_test_set_avoidleakage.py
+++ random_test_set_avoidleakage.py
@@ -15,14 +15,25 @@ from pytorch_lightning.utilities.seed import seed_everything
 import shutil
 import debugger
 
+# def gather_job(pdb_dir):
+#     pdb_paths = []
+#     for f_path in os.listdir(pdb_dir):
+#         if f_path.endswith('.pdb'):
+#             pdb_path = os.path.join(pdb_dir, f_path)
+#             pdb_paths.append(pdb_path)
+    
+#     return pdb_paths
+
 def gather_job(pdb_dir):
-    pdb_paths = []
+    pdb_names = []
     for f_path in os.listdir(pdb_dir):
         if f_path.endswith('.pdb'):
-            pdb_path = os.path.join(pdb_dir, f_path)
-            pdb_paths.append(pdb_path)
+            pdb_name = f_path[:5]
+            if pdb_name not in pdb_names:
+                pdb_names.append(pdb_name)
     
-    return pdb_paths
+    return pdb_names
+
 
 def bool_type(bool_str: str):
     bool_str_lower = bool_str.lower()
@@ -41,29 +52,46 @@ def main(args):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    output_test = os.path.join(output_dir,"test")
+    output_train = os.path.join(output_dir,"train")
+    if not os.path.exists(output_test):
+        os.makedirs(output_test)
+    if not os.path.exists(output_train):
+        os.makedirs(output_train)
+
     jobs = gather_job(args.pdb_path)
     logging.info(f'got {len(jobs)} jobs...')
     # Get input 
 
 
-    top_k = 400
+    top_k = 40
     prob = torch.rand(len(jobs))
     _, indexes = torch.topk(prob, top_k)
     indexes = indexes.tolist()
-    topk_jobs = [jobs[i] for i in indexes]
-    for job in topk_jobs:
-        f_path = os.path.basename(job)
-        name = f_path[:args.name_length]
+    topk_test = [jobs[i] for i in indexes]
+    topk_train = [x for x in jobs if x not in topk_test] 
+    post_string = ["_sample0", "_sample1", "_sample2"]
+    for job in topk_test:
+        for i in range(3):
+            name = job+post_string[i]
+            src = os.path.join(args.pdb_path,f"{name}.pdb")
+            tgt = os.path.join(output_test,f"{name}.pdb")
+            
+            # print(f"#################")
+            # print(f">>> treating: {name}")
         
-        print(f"#################")
-        print(f">>> treating: {name}")
+            shutil.copyfile(src, tgt)
+    
+    for job in topk_train:
+        for i in range(3):
+            name = job+post_string[i]
+            src = os.path.join(args.pdb_path,f"{name}.pdb")
+            tgt = os.path.join(output_train,f"{name}.pdb")
+            
+            # print(f"#################")
+            # print(f">>> treating: {name}")
         
-        dst = os.path.join(
-            output_dir,
-            f"{name}.pdb"
-        )
-        shutil.copyfile(job, dst)
-
+            shutil.copyfile(src, tgt)
         # # 2nd option
         # shutil.copy(src, dst) 
 
@@ -89,5 +117,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
 
-    #usage
-    # python random_test_set.py /nfs/work04/chuanrui/data/sampled_protein/sampling --output_dir /nfs/work04/chuanrui/data/subset_400
+    # usage
+    # python /home/Xcwang/scratch/beluga/JointProteinFolding/random_test_set_avoidleakage.py /nfs/work04/chuanrui/data/sampled_protein/sampling --output_dir /nfs/work04/chuanrui/data/subset_1200_noleakage
